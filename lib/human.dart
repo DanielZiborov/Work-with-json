3b71351429ff276b13a1d42e4e37a75b9688db71
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-03 14:22:34 +0300

    human

---
+++ lib/human.dart
@@ -0,0 +1,13 @@
+class Human {
+  String name;
+  String surname;
+  int age;
+  List<Address> addreses;
+
+  Human({
+    required this.name,
+    required this.surname,
+    required this.age,
+    required this.addreses,
+  });
+}
