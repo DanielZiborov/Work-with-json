dd3847e976693c8885daa0f0a6fda9a30e74fbcc
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-03 20:01:04 +0300

    example

--- lib/example.dart
+++ lib/example.dart
@@ -5,6 +5,63 @@ class Example extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: JsonExampleProvider(
+          coder: JsonExampleCoder(),
+          child: const ButtonWidget(),
+        ),
+      ),
+    );
   }
+}
+
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Кодировать"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Декодировать"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class JsonExampleProvider extends InheritedWidget {
+  final JsonExampleCoder coder;
+  const JsonExampleProvider({
+    super.key,
+    required this.coder,
+    required super.child,
+  });
+
+  static JsonExampleProvider? watch(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<JsonExampleProvider>();
+  }
+
+  static JsonExampleProvider? read(BuildContext context) {
+    final widget = context.getElementForInheritedWidgetOfExactType<JsonExampleProvider>()?.widget;
+    return widget is JsonExampleProvider? widget: null;
+  }
+
+  @override
+  bool updateShouldNotify(JsonExampleProvider oldWidget) {
+    return true;
+  }
+}
+
+class JsonExampleCoder{
+  void encode() {}
+  void decode() {}
 }
