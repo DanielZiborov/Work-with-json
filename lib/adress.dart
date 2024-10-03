0fbd71c2745a02e7ce04a2960c7c55c935b5e2ba
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-03 20:05:51 +0300

    adress

---
+++ lib/adress.dart
@@ -0,0 +1,13 @@
+class Adress {
+  final String city;
+  final String street;
+  final String house;
+  final int flat;
+  
+  Adress({
+    required this.city,
+    required this.street,
+    required this.house,
+    required this.flat,
+  });
+}
